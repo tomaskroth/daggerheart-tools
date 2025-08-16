95d35a0102b35775131ce1a54435b59bcbe2c8cb
tomaskroth <tomaskroth@gmail.com>
2025-08-16 17:33:36 +0200

    Fixing wrong backend url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import ServerStatusGate from './components/ServerStatusGate';
 import './App.css';
 
-const SERVER_URL = 'http://localhost:8080/api';
-// const SERVER_URL = 'https://daggerheart-tools-4v1t.onrender.com/api';
+// const SERVER_URL = 'http://localhost:8080/api';
+const SERVER_URL = 'https://daggerheart-tools-4v1t.onrender.com/api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
